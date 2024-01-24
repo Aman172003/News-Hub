79f15210e474558f463b2875c48a60463f5943ab
Aman Kumar Rai <tej171995@gmail.com>
2024-01-25 00:00:44 +0530

    added routed

--- src/App.js
+++ src/App.js
@@ -2,15 +2,117 @@ import "./App.css";
 import React, { Component } from "react";
 import Navbar from "./components/Navbar";
 import News from "./components/News";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 // rcc
 // react class based component
 export default class App extends Component {
   render() {
     return (
+      // key dene se page rerender ho jaega
+      // agar key nhi dete to alag category par jaane par bhi page load nhi hota
       <div>
-        <Navbar />
-        <News pageSize={5} country="in" category="sports" />
+        <Router>
+          <Navbar />
+          <Routes>
+            <Route
+              exact
+              path="/"
+              element={
+                <News
+                  key="general"
+                  pageSize={5}
+                  country="in"
+                  category="general"
+                />
+              }
+            ></Route>
+            <Route
+              exact
+              path="/business"
+              element={
+                <News
+                  key="business"
+                  pageSize={5}
+                  country="in"
+                  category="business"
+                />
+              }
+            ></Route>
+            <Route
+              exact
+              path="/entertainment"
+              element={
+                <News
+                  key="entertainment"
+                  pageSize={5}
+                  country="in"
+                  category="entertainment"
+                />
+              }
+            ></Route>
+            <Route
+              exact
+              path="/general"
+              element={
+                <News
+                  key="general"
+                  pageSize={5}
+                  country="in"
+                  category="general"
+                />
+              }
+            ></Route>
+            <Route
+              exact
+              path="/health"
+              element={
+                <News
+                  key="health"
+                  pageSize={5}
+                  country="in"
+                  category="health"
+                />
+              }
+            ></Route>
+            <Route
+              exact
+              path="/science"
+              element={
+                <News
+                  key="science"
+                  pageSize={5}
+                  country="in"
+                  category="science"
+                />
+              }
+            ></Route>
+            <Route
+              exact
+              path="/sports"
+              element={
+                <News
+                  key="sports"
+                  pageSize={5}
+                  country="in"
+                  category="sports"
+                />
+              }
+            ></Route>
+            <Route
+              exact
+              path="/technology"
+              element={
+                <News
+                  key="technology"
+                  pageSize={5}
+                  country="in"
+                  category="technology"
+                />
+              }
+            ></Route>
+          </Routes>
+        </Router>
       </div>
     );
   }
